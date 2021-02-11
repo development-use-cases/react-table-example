0bfd7b48434142b895cc64e7bfe31f99bb5acc73
Dmitry Barskov <dmitry.barskov@flatstack.com>
2021-02-12 00:17:32 +0300

    draft sorting table

--- app/javascript/components/table/SortedTable.jsx
+++ app/javascript/components/table/SortedTable.jsx
@@ -5,20 +5,39 @@ import Header from "./Header";
 
 const ColumnName = ({ label, sorting, onClick }) => (
   <span style={{ cursor: "pointer" }} onClick={() => onClick(label)}>
-    {label} {sorting == "asc" ? "▼" : "▲"}
+    {label} {sorting === "asc" ? "▼" : "▲"}
   </span>
 );
+
+const sortingFunction = (sorting) => (a, b) => {
+  for (let order of sorting) {
+    if (a[order.label] === b[order.label]) {
+      return 0;
+    }
+
+    if (a[order.label] < b[order.label]) {
+      return sorting == "asc" ? -1 : 1;
+    }
+
+    return sorting == "asc" ? 1 : -1;
+  }
+};
 
 const opposite = (sorting) => (sorting === "asc" ? "desc" : "asc");
 
 const SortedTable = ({ data, columns }) => {
   const [sorting, setSorting] = useState([]);
+  const [rows, setRows] = useState([]);
 
   useEffect(() => {
     setSorting(
       columns.map((columnLabel) => ({ label: columnLabel, sorting: "asc" }))
     );
   }, [columns]);
+
+  useEffect(() => {
+    setRows(data.slice());
+  }, [data]);
 
   const changeSorting = (columnLabel) => {
     let newSorting = sorting.map((column) =>
@@ -30,6 +49,8 @@ const SortedTable = ({ data, columns }) => {
         : column
     );
     setSorting(newSorting);
+    let newRows = rows.sort(sortingFunction(newSorting));
+    setRows(newRows);
   };
 
   const columnNames = sorting.map((col) => (
@@ -39,7 +60,7 @@ const SortedTable = ({ data, columns }) => {
   return (
     <table>
       <Header columns={columnNames} />
-      <Body columns={columns} data={data} />
+      <Body columns={columns} data={rows} />
     </table>
   );
 };
